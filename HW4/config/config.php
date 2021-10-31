970bc9e8215de3068fa6ce454c85ee3c54af4772
Nikolay Vanzhin <72859629+Vanzhin@users.noreply.github.com>
2021-10-31 11:00:18 +0500

    Revert "HW4"

--- HW4/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('ROOT', dirname(__DIR__));
-define('DS', DIRECTORY_SEPARATOR);
-define('CONTROLLER_NAMESPACE', "app\\controllers\\");
-define('VIEWS_DIR', "../views/");
-define('ITEMS_PER_PAGE', 2);
-
-
